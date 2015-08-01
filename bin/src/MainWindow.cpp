8e0eb452d1cafa5af5eea463bcb404d9f6b88c24
Hlib Budylo <h.budylo@gmail.com>
2015-08-02 00:05:05 +0300

    Add chess plate image to Main Window

--- bin/src/MainWindow.cpp
+++ bin/src/MainWindow.cpp
@@ -4,6 +4,12 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    QString filePath = QApplication::applicationDirPath();
+    filePath.append("/res/Images/pic1.png");
+
+    ui.label->setScaledContents(1);
+    ui.label->setPixmap(QPixmap(filePath));
 }
 
 MainWindow::~MainWindow()
