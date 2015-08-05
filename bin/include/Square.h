8761e5e0cae005c182d069aff4916c44ad7067a8
Hlib Budylo <h.budylo@gmail.com>
2015-08-06 00:57:36 +0300

    implement pieces movement

--- bin/include/Square.h
+++ bin/include/Square.h
@@ -2,16 +2,22 @@
 
 class Piece;
 
-class Square
+class Square: public QObject
 {
+   Q_OBJECT
 public:
     Square();
     ~Square();
 
     QPushButton* getButton() const;
     Piece * setPiece(Piece * piece);
+    Piece * getPiece();
+
+public slots:
+    void actionOnClick();
 
 private:
     QPushButton *m_button;
     Piece * m_piece;
+    static Square* m_square; //contains selected square on board
 };
--- bin/src/Square.cpp
+++ bin/src/Square.cpp
@@ -1,12 +1,15 @@
 #include <Square.h>
 #include <Piece.h>
 
+Square* Square::m_square = NULL;
+
 Square::Square()
 {
     m_button = new QPushButton();
     m_piece = NULL;
     m_button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     m_button->setStyleSheet("background-color: rgba(0, 0, 0, 0)");
+    connect(m_button, SIGNAL (released()), this, SLOT (actionOnClick()));
 }
 
 Square::~Square()
@@ -30,4 +33,25 @@ Piece* Square::setPiece(Piece *piece)
 
     m_piece = piece;
     return ret;
+}
+void Square::actionOnClick()
+{
+    if (Square::m_square == this)
+        return;
+
+    if (Square::m_square)
+    {
+        if (m_piece)
+            m_piece = NULL;
+
+        setPiece(Square::m_square->getPiece());
+        Square::m_square->setPiece(NULL);
+        Square::m_square = NULL;
+    }
+    else if (m_piece)
+        Square::m_square = this;
+}
+Piece* Square::getPiece()
+{
+    return m_piece;
 }
